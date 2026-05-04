902cbed5f211e041eb14d7fc300b540195ada6b8
elfisania <elfisania06@gmail.com>
2026-05-04 16:58:52 +0700

    Menambahkan fitur pada grafik dengan filter bulan di dashboard

--- resources/views/rekon-kas/index.blade.php
+++ resources/views/rekon-kas/index.blade.php
@@ -119,7 +119,7 @@
                 </div>
             @endif
         @else
-            <div class="px-4 py-6 text-sm text-slate-500">
+            <div class="px-4 py-6 text-sm text-slate-500 text-center">
                 Belum ada data rekon kas.
             </div>
         @endif
